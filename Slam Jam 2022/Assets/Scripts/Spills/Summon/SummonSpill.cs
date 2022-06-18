5e59d4e65a3b6000f026aa4e7f60d24c524ddcb7
zachary newman <zach.newman981@yahoo.com.au>
2022-06-18 12:17:04 +0930

    Low Poly assets: Fixed materials

--- Slam Jam 2022/Assets/Scripts/Spills/Summon/SummonSpill.cs
+++ Slam Jam 2022/Assets/Scripts/Spills/Summon/SummonSpill.cs
@@ -9,9 +9,6 @@ public class SummonSpill : GenericSpill
 	[SerializeField] Summonable summon;
 
 
-	[Tooltip("The Lifetime of the summon, leave as -1 for infinite")]
-	[SerializeField] float summonLifeTime = -1;
-
 	[Tooltip("The max amount of summons that can be active at once, leave as -1 for infinite")]
 	[SerializeField] int maxSummons = -1;
 	int currentSummons = 0;
@@ -19,16 +16,12 @@ public class SummonSpill : GenericSpill
 	[Tooltip("How many get summoned at once")]
 	[SerializeField] int numToSummon = 1;
 
-	[Space]
-
-	[SerializeField] bool parentToPlayer;
-
 	public override bool CanCastSpell(PlayerStats caster)
 	{
-		if (base.CanCastSpell(caster))
+		if (maxSummons > 0 && currentSummons >= maxSummons)
 			return false;
 
-		if (maxSummons > 0 && currentSummons >= maxSummons)
+		if (base.CanCastSpell(caster))
 			return false;
 
 		return true;
@@ -39,7 +32,8 @@ public class SummonSpill : GenericSpill
 		if (!base.Cast(caster))
 			return false;
 
-
+		var inst = Instantiate(summon);
+		inst.Initialise(caster, this);
 
 		return true;
 	}
