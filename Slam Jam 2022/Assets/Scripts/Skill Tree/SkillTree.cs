ac3c67c5d1668d1f76bc6ef4039241cbe1b58df2
Joshua Tan <joshuatan0806@gmail.com>
2022-06-18 22:15:14 +0930

    more skill tree shiz

--- Slam Jam 2022/Assets/Scripts/Skill Tree/SkillTree.cs
+++ Slam Jam 2022/Assets/Scripts/Skill Tree/SkillTree.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillTree : MonoBehaviour
 {
     [SerializeField] GameObject Node;
-    [SerializeField] GameObject NodeHolder;
+    [SerializeField] RectTransform NodeHolder;
+    [SerializeField] ScrollRect scrollRect;
 
     [SerializeField] float minScale;
     [SerializeField] float maxScale;
     [SerializeField] float scrollMultiplier;
+
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
 
     private void Awake()
     {
@@ -42,6 +49,7 @@ public class SkillTree : MonoBehaviour
     private void Update()
     {
         Zoom();
+        Clamp();
     }
 
     void Zoom()
@@ -53,5 +61,26 @@ public class SkillTree : MonoBehaviour
         scale = Mathf.Clamp(scale, minScale, maxScale);
 
         NodeHolder.transform.localScale = scale * Vector3.one;
+    }
+
+    void Clamp()
+    {
+        Vector3 oldPos = NodeHolder.anchoredPosition;
+        float zoom = NodeHolder.localScale.x;
+        float zoomSquared = zoom * zoom;
+
+        Vector3 newPos = new Vector3();
+
+        newPos.x = Mathf.Clamp(oldPos.x, minX * zoomSquared, maxX * zoomSquared);
+        newPos.y = Mathf.Clamp(oldPos.y, minY * zoomSquared, maxY * zoomSquared);
+
+        if (newPos.x != oldPos.x)
+            scrollRect.velocity = new Vector3(0, scrollRect.velocity.y);
+
+        if (newPos.y != oldPos.y)
+            scrollRect.velocity = new Vector3(scrollRect.velocity.x, 0);
+
+
+        NodeHolder.anchoredPosition = newPos;
     }
 }
