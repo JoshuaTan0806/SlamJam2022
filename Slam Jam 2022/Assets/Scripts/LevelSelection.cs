0dd912802a8a23de4959b1fe231320d146b15a44
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2022-06-20 11:57:09 +0930

    InfiniCloud asset pack added. Map and level loading. NPC narratives with buttons to screen on airship.

--- Slam Jam 2022/Assets/Scripts/LevelSelection.cs
+++ Slam Jam 2022/Assets/Scripts/LevelSelection.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class LevelSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Loadlevel1()
     {
-        
+
     }
+
+    
 }
