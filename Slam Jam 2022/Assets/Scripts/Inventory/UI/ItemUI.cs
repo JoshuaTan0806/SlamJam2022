e1d334ef9c0a53f5edd3c24c35f2d0c3823a6a1c
Luke Jones <nnblc565@gmail.com>
2022-06-22 19:54:11 +0930

    Fixed item stat rolling. Started on UI

---
+++ Slam Jam 2022/Assets/Scripts/Inventory/UI/ItemUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Items;
+
+public class ItemUI : MonoBehaviour
+{
+    public ItemData item = null;
+
+    public void RefreshUI()
+    {
+
+    }
+}
