df7b0a5709e34b2951c770d31f442cc915c4ac93
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2022-06-24 17:18:33 +0930

    close buttons on airship narrative

--- Slam Jam 2022/Assets/Scripts/NPCInteract.cs
+++ Slam Jam 2022/Assets/Scripts/NPCInteract.cs
@@ -7,13 +7,20 @@ public class NPCInteract : MonoBehaviour
     [SerializeField] private GameObject NPCPanel;
     [SerializeField] private GameObject player;
     [SerializeField] private Outline npcoutline;
+    [SerializeField] private GameObject[] otherPanels;
 
     private void OnMouseDown()
     {
         float distance = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
         Debug.Log("distance" + distance);
         if (distance < 2.5f)
+        {
             NPCPanel.SetActive(true);
+            foreach (GameObject panel in otherPanels)
+            {
+                panel.SetActive(false);
+            }
+        }
         else
         {
             NPCPanel.SetActive(false);
